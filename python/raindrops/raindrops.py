43aad176d815b1452832fdce2d9cb1e99c533a15
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-02-10 18:22:28 -0300

    Refactory Raindrops in Python

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,11 +1,11 @@
 def convert(number):
     result = ""
     if number % 3 == 0:
-        result = result + "Pling"
+        result += "Pling"
     if number % 5 == 0:
-        result = result + "Plang"
+        result += "Plang"
     if number % 7 == 0:
-        result = result + "Plong"
-    if result == "":
+        result += "Plong"
+    if number % 3 != 0 and number % 5 != 0 and number % 7 != 0:
         result = str(number)
     return result
