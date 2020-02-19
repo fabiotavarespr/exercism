9a04d1ea989a4c375646af9b468f722c8f4c6af8
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-02-18 22:11:43 -0300

    Versionando codigo

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 main () {
-    if [ "$#" -ne 1 ]; then
+    if (( "$#" != 1 )); then
         echo "Usage: ./error_handling <greetee>"
         exit 1
     fi
--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,3 +1,14 @@
+/*
+Package twofer implements
+
+Two-fer or 2-fer is short for two for one. One for you and one for me.
+
+Given a name, return a string with the message:
+One for X, one for me.
+Where X is the given name.
+However, if the name is missing, return the string
+One for you, one for me.
+*/
 package twofer
 
 import "fmt"
