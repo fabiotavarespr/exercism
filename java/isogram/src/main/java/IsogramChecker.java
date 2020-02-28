4155c2ebf5c0dd72df582573a76274fcb2e89eca
Fabio Tavares Pereira Rego <fabiotavarespr@gmail.com>
2020-02-28 14:47:25 -0300

    Isogram em Java

---
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -0,0 +1,7 @@
+class IsogramChecker {
+
+    boolean isIsogram(String phrase) {
+        phrase = phrase.toLowerCase().replaceAll("\\W", "");
+        return phrase.chars().distinct().count() == phrase.length();
+    }
+}
